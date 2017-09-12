427ab4f4daca4bd9861fe746e4821efebf20a0ef
OlegKlimec <olegklimec@gmail.com>
2017-09-13 01:09:06 +0200

    mitsin v 12/9/17
    mitsin v 12/9/17

--- mitsin/js/script.js
+++ mitsin/js/script.js
@@ -1,17 +1,16 @@
-﻿
-  
-new WOW({
-offset:300
-	
-}).init(
+﻿$(document).ready(function(){
+
+$(".navbar").removeClass("default");
+$(window).scroll(function(){
+	if ($(this).scrollTop()>30) {
+		$(".navbar").addClass("default").fadeIn('fast');
+
+	}
+	else{
+		$(".navbar").removeClass("default").fadeIn('fast');
+	};
+
+});
 
 
-);
-
-
-/*$('#navbar').scrollspy();*/
-
-	
-	
-
-     
+});
